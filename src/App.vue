8ae0013e17adb0544af85ab4a355dce325080dda
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-14 09:47:25 -0500

    implement header & description component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-
+import Header from './components/Header.vue';
+import Description from './components/Description.vue';
 </script>
 
 <template>
-
-
+  <Header />
+  <Description />
 </template>
 
 <style scoped>
