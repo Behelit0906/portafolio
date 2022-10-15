eaa8760ddf994c6fabcf0efdab98662b81be063d
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-15 11:00:00 -0500

    add skillTable component to App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
 import Description from './components/Description.vue';
+import SkillsTable from './components/SkillsTable.vue';
 </script>
 
 <template>
   <Header />
   <Description />
+  <SkillsTable />
 </template>
 
 <style scoped>
