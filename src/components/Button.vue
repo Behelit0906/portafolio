92272dc48753888654f2002a40594f86c994a33b
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-16 16:47:06 -0500

    create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{
+    name: string
+    link: string
+}>()
+</script>
+
+<template>
+    <a :href="props.link" class="button-link">{{props.name}}</a>
+</template>
+
+<style scoped>
+.button-link {
+    display: block;
+    width: 40%;
+    padding: 10px;
+    text-align: center;
+    color: #fff;
+    background-color: var(--main-color);
+    border: 1px solid #fff;
+    border-radius: 5px;
+}
+
+.button-link:hover {
+    color: var(--main-color);
+    background-color: #fff;
+    border: 1px solid var(--main-color);
+}
+</style>
