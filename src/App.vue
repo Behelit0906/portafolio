6b408382a4ad78cc574810e2bba38684d28ce168
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-16 16:48:55 -0500

    implement worksTable component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Header from './components/Header.vue';
 import Description from './components/Description.vue';
 import SkillsTable from './components/SkillsTable.vue';
+import WorksTable from './components/WorksTable.vue';
 </script>
 
 <template>
   <Header />
   <Description />
   <SkillsTable />
+  <WorksTable />
 </template>
 
 <style scoped>
